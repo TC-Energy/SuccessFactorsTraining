9cdb72af7cb34af13161ff5c09eb5790cbb8f910
Jai_Patel <jai_patel@tcenergy.com>
2021-02-19 15:57:28 -0700

    - Added procedure to add info to DB - Cleaned up some code - Created getAD details class

---
+++ SuccessFactors/SuccessFactors/Models/GetAD_Details.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.DirectoryServices.AccountManagement;
+
+namespace SuccessFactors.Models
+{
+    public class GetAD_Details
+    {
+        public string loginname { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string name { get; set; }
+        public string eMail { get; set; }
+        public int emp_id { get; set; }
+    }
+}
