d154099a2aae658ee28eeedb7f83a0d7b5dd31e9
Sam Bedu-Annan <sam_bedu-annan@tcenergy.com>
2021-02-20 23:42:09 -0700

    Additional modification in external users controller

--- SuccessFactors/SuccessFactors/Controllers/ExtHomeController.cs
+++ SuccessFactors/SuccessFactors/Controllers/ExtHomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SuccessFactors.Models;
 using static SuccessFactors.Models.StudentInfo;
 
 namespace SuccessFactors.Controllers
