d8dc4d6ef0319a69e2bb52bbfe99b4fae0211c5c
Sam Bedu-Annan <sam_bedu-annan@tcenergy.com>
2021-02-20 23:39:27 -0700

    Modified external users controller

--- SuccessFactors/SuccessFactors/Controllers/ExtHomeController.cs
+++ SuccessFactors/SuccessFactors/Controllers/ExtHomeController.cs
@@ -9,9 +9,33 @@ namespace SuccessFactors.Controllers
     public class ExtHomeController : Controller
     {
         // GET: ExtHome
-        public ActionResult Index()
+        public ActionResult ExtHome_Index()
         {
             return View();
+        }
+
+        public ActionResult ExtStudents()
+        {
+            ViewBag.Message = "Your student page.";
+            var data = load_employees();
+            List<StudentInfo> extstudents = new List<StudentInfo>();
+
+            foreach (var row in data)
+            {
+                extstudents.Add(new StudentInfo
+                {
+                    id = row.id,
+                    first_name = row.first_name,
+                    last_name = row.last_name,
+                    email = row.email,
+                    courses = row.courses,
+                    completion_status = row.completion_status,
+                    employeenumber = row.employeenumber,
+                    section = row.section
+                });
+            }
+
+            return View(extstudents);
         }
     }
 }
