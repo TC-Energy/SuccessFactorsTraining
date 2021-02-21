65245c1639a801915b8be513dd60697d0c4b2f9a
Sam Bedu-Annan <sam_bedu-annan@tcenergy.com>
2021-02-20 23:37:26 -0700

    Added new controller for external users

---
+++ SuccessFactors/SuccessFactors/Controllers/ExtHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuccessFactors.Controllers
+{
+    public class ExtHomeController : Controller
+    {
+        // GET: ExtHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
