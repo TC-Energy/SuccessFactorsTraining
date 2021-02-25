1295b6276d8e75430a42f562d95a21144520c3a9
Jai_Patel <jai_patel@tcenergy.com>
2021-02-25 09:11:23 -0700

    Cleaned up the code

--- SuccessFactors/SuccessFactors/Controllers/HomeController.cs
+++ SuccessFactors/SuccessFactors/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace SuccessFactors.Controllers
             g1.eMail = UserPrincipal.Current.EmailAddress;
             g1.emp_id = Int32.Parse(UserPrincipal.Current.EmployeeId);
 
-            SQLConnection.checkUserExists("checkUserExists", g1.emp_id, g1.firstname, g1.lastname, g1.name, "asd", "Admin", g1.eMail);
+            SQLConnection.checkUserExists("checkUserExists", g1.emp_id, g1.firstname, g1.lastname, g1.name, "", "", g1.eMail);
 
             return View();
         }
@@ -39,14 +39,10 @@ namespace SuccessFactors.Controllers
             {
                 students.Add(new StudentInfo
                 {
-                    id = row.id,
+                    emp_id = row.emp_id,
                     first_name = row.first_name,
                     last_name = row.last_name,
-                    email = row.email,
-                    courses = row.courses,
-                    completion_status = row.completion_status,
-                    employeenumber = row.employeenumber,
-                    section = row.section
+                    email = row.email
                 });
             }
 
